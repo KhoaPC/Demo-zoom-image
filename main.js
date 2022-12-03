b536dbe3243e7f0e1f5ff92b6844c623f65298bd
KhoaPC <luongvankhoapc@gmail.com>
2022-12-03 11:49:29 +0700

    First Commit

--- main.js
+++ main.js
@@ -1,24 +1,22 @@
 const CONST_WRAPPER_WIDTH = 600;
-const CONST_SCALE_SEED = 0.3;
 const imgNaturalWidth = img.naturalWidth;
 const scaleFactorMax = imgNaturalWidth / CONST_WRAPPER_WIDTH;
+// zoom(document.querySelector("#slide"), 0.3);
+const c = console.log;
+zoom2(container, 0.3);
 
 // #slide
-function zoom(overlay, factor) {
+function zoom(elm, scaleSeed) {
   let scale = 1;
-  const target = overlay.children[0].querySelector("#slide");
   const pos = { x: 0, y: 0 };
   const zoom_target = { x: 0, y: 0 };
   const cursor = { x: 0, y: 0 };
-
-  target.addEventListener("mousedown", mouseDown);
+  elm.addEventListener("mousedown", mouseDown);
 
   function mouseDown(e) {
-    const naturalWidth = target.children[0].naturalWidth;
-    const currentWidth = target.children[0].getBoundingClientRect().width;
     // Xác định vị trí con trỏ
-    cursor.x = e.offsetX;
-    cursor.y = e.offsetY;
+    cursor.x = e.pageX;
+    cursor.y = e.pageY;
     e.preventDefault();
 
     // Xác định vị trí được zoom trong ảnh
@@ -26,10 +24,10 @@ function zoom(overlay, factor) {
     zoom_target.y = (cursor.y - pos.y) / scale;
 
     // scale
-    if (e.shiftKey || e.ctrlKey) scale -= factor;
+    if (e.shiftKey || e.ctrlKey) scale -= scaleSeed;
     else {
       if (scale < scaleFactorMax) {
-        scale += factor;
+        scale += scaleSeed;
       } else {
         alert("Max width");
       }
@@ -46,8 +44,66 @@ function zoom(overlay, factor) {
       pos.y = 0;
     }
 
-    target.style.transform = `translate(${pos.x}px, ${pos.y}px) scale(${scale})`;
+    elm.style.transform = `translate(${pos.x}px, ${pos.y}px) scale(${scale})`;
   }
-}
+} // zoom
 
-zoom(document.querySelector(".overlay"), 0.3);
+function zoom2(container, factor) {
+  let scale = 1;
+  const slide = container.querySelector("#slide");
+  const pos = { x: 0, y: 0 };
+  const zoom_target = { x: 0, y: 0 };
+  const cursor = { x: 0, y: 0 };
+  const size = { x: 0, y: 0 };
+  const containerComputed = container.getBoundingClientRect();
+
+  slide.addEventListener("mousedown", mouseDown);
+
+  function mouseDown(e) {
+    const slideComputed = slide.getBoundingClientRect();
+    e.preventDefault();
+
+    size.x = slideComputed.x;
+    size.y = slideComputed.y;
+
+    // Xác định vị trí con trỏ
+    cursor.x = e.pageX - containerComputed.left;
+    cursor.y = e.pageY - containerComputed.top;
+
+    // Xác định vị trí được zoom trong ảnh
+    zoom_target.x = (cursor.x - pos.x) / scale;
+    zoom_target.y = (cursor.y - pos.y) / scale;
+
+    // Giữ Shift/ Ctrl thì zoom out
+    const zoomOut = e.shiftKey || e.ctrlKey;
+
+    // Calculate the scale
+    scale += zoomOut ? -factor : factor;
+
+    // maxScale = scaleFactorMax; minScale = 1
+    scale = Math.max(1, Math.min(scaleFactorMax, scale));
+
+    // Xác định vị trí sau khi zoom
+    pos.x = -zoom_target.x * scale + cursor.x;
+    pos.y = -zoom_target.y * scale + cursor.y;
+
+    if (scale === 1) {
+      pos.x = 0;
+      pos.y = 0;
+    }
+
+    if (size.x > 5 && zoomOut) {
+      pos.x = 0;
+      console.log("hi");
+    }
+
+    if (size.y > 5 && zoomOut) {
+      pos.y = 0;
+      console.log("hello");
+    }
+
+    // Cho ảnh vào giữa khi zoom out
+
+    slide.style.transform = `translate(${pos.x}px, ${pos.y}px) scale(${scale})`;
+  }
+} // zoom2
